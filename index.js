df95b7d06d0de9e85f56bb15dd1bb5050ec6285c
3mrePala <palaemre27@gmail.com>
2022-09-13 10:42:33 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20
+
+// MOVING SHAPES
+
+function moveBackground (event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+    }
+}
+
+
+// This is too change the theme between black/white
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+    document.body.classList += " dark-theme"
+    }
+    else
+    document.body.classList.remove ("dark-theme")
+}  
+
+
+// To function the modal and let viewers send emails through
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+    emailjs
+      .sendForm(
+        "service_yml4u4n",
+        "template_s3chdi9",
+        event.target,
+        "aRc6oSK-1-hlH9Buf"
+      )
+      .then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+      })
+      .catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+          "The email service is temporarily unavailable. Please contact me directly on palaemre27@gmail.com"
+        );
+      });
+  }
+
+// To Open and Close the MODAL
+
+function toggleModal() {
+    if (isModalOpen) {
+      isModalOpen = false;
+      return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+  }
